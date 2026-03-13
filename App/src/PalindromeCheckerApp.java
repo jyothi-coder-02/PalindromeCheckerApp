792391212ea9ffa6d548ef3e6d4679fb1c65bbec
jyothi-coder-02 <jyothireddydevara@gmail.com>
2026-03-13 10:21:06 +0530

    [jyothi] Added UC5

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
@@ -5,25 +7,22 @@ public class PalindromeCheckerApp {
         // Original string
         String str = "madam";
 
-        // Convert string to character array
-        char[] chars = str.toCharArray();
+        // Create a stack
+        Stack<Character> stack = new Stack<>();
 
-        int start = 0;
-        int end = chars.length - 1;
-        boolean isPalindrome = true;
-
-        // Two-pointer comparison
-        while (start < end) {
-            if (chars[start] != chars[end]) {
-                isPalindrome = false;
-                break;
-            }
-            start++;
-            end--;
+        // Push characters into stack
+        for (int i = 0; i < str.length(); i++) {
+            stack.push(str.charAt(i));
         }
 
-        // Display result
-        if (isPalindrome) {
+        // Pop characters to build reversed string
+        String reversed = "";
+        while (!stack.isEmpty()) {
+            reversed = reversed + stack.pop();
+        }
+
+        // Compare original and reversed string
+        if (str.equals(reversed)) {
             System.out.println(str + " is a Palindrome");
         } else {
             System.out.println(str + " is not a Palindrome");
